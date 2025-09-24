a46f1374c9fb89e443e078a7925180ccbfcd9051
JesusCMM-LLMM <jesuscmoramesa@gmail.com>
2025-09-24 20:54:51 +0200

    REVAMP

--- BLOQUE_01/UD_1_2/Ejercicio_guiado_1_2.py
+++ BLOQUE_01/UD_1_2/Ejercicio_guiado_1_2.py
@@ -4,6 +4,8 @@ edad = int(input("¿Cuántos años tienes? "))
 altura = float(input("¿Cuánto mides? "))
 
 
-estudia = input("¿Estudias actualmente? (y/n) ").lower() == "y"
+estudia = input("¿Estudias actualmente? (y/n) ") == "y"
 
 print(f"Me llamo {nombre}, tengo {edad} años, mido {altura} y actualmente estudio: {estudia}")
+
+
