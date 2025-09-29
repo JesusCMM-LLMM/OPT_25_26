056c3205fa80b3ab701b60df614d68b86afc9949
JesusCMM-LLMM <jesuscmoramesa@gmail.com>
2025-09-29 11:57:05 +0200

    guiado 1.2 corregido

--- BLOQUE_01/UD_1_2/Ejercicio_guiado_1_2.py
+++ BLOQUE_01/UD_1_2/Ejercicio_guiado_1_2.py
@@ -4,8 +4,15 @@ edad = int(input("¿Cuántos años tienes? "))
 altura = float(input("¿Cuánto mides? "))
 
 
-estudia = input("¿Estudias actualmente? (y/n) ") == "y"
+estudia = input("¿Estudias actualmente? (y/n) ") # == "y"
 
-print(f"Me llamo {nombre}, tengo {edad} años, mido {altura} y actualmente estudio: {estudia}")
+if (estudia == "y"):
+    resp = "sí"
+elif (estudia == "n"):
+    resp = "no"
+else:
+    resp = "no se si"
+
+print(f"Me llamo {nombre}, tengo {edad} años, mido {altura} y actualmente {resp} estudio")
 
 
