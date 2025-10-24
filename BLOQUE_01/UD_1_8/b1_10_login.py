b99292f90dd28a2eb6ab3cf618a1f4b92fa5a2b6
JesusCMM-LLMM <jesuscmoramesa@gmail.com>
2025-10-24 13:00:50 +0200

    1draft

--- BLOQUE_01/UD_1_8/b1_10_login.py
+++ BLOQUE_01/UD_1_8/b1_10_login.py
@@ -1,46 +1,8 @@
 """
- Requisitos del programa
-El programa debe mostrar un menú principal con tres opciones:
-
-[1]. Registrarse → El usuario crea un nuevo nombre de usuario y contraseña.
-[2]. Iniciar sesión → El usuario introduce su nombre y contraseña para acceder.
-[3]. Salir → Finaliza el programa.
-Registro de usuario:
-
-La información del usuario se almacenará en variables:
-
-identifier es el email del usuario de usuario.
-password es la contraseña asociada.
-
-Antes de aceptar el identificador, debes validarla con estas reglas:
-    Mínimo 3 caracteres.
-    Contener al menos @.
-    Contener al menos alguna de las siguientes extensiones .com, .es, .net.
-    No debe contener símbolos especiales como (!@#$%&*?, etc.).
-
-
-Antes de aceptar la contraseña, debes validarla con estas reglas:
-    Mínimo 8 caracteres.
-    Contener al menos una mayúscula.
-    Contener al menos un número.
-    Contener al menos un símbolo especial (!@#$%&*?, etc.).
-
-Si no cumple las reglas → muestra un mensaje de error y vuelve a pedir el usuario o la contraseña.
-
-Inicio de sesión:
-
-Verifica si el usuario (email) existe.
-
-Si existe y la contraseña es correcta → muestra “Acceso concedido ✅”.
-
-Si el usuario no existe o la contraseña es incorrecta → muestra “Acceso denegado ⛔”.
-
-El usuario tendrá un máximo de 3 intentos para introducir su contraseña correctamente.
-
-Si falla las 3 veces seguidas → mostrar “Demasiados intentos fallidos 🚫. Regresando al menú principal.” y volver al menú.
-El programa debe ejecutarse en bucle hasta que el usuario elija la opción Salir.
-
+ Este es el menú sin funciones. Lo primero, para no usar funciones y que solo se cierre al usar la opcion 3, el menú está dentro de un while True.
+ Segundo, los arrays vacíos van fuera del bucle para que no se reinicien en cada vuelta.
 """
+
 usu_reg = []
 cont_reg = []
 
@@ -53,12 +15,14 @@ while True:
     print("| [3].- Salir                          |")
     print("|######################################|")
 
+    # La opción como una variable que va a leerse en el if/elif/else
     opt = int(input("       Introduce tu opción: "))
 
     if (opt == 1):
         username = input("Introduce usuario a registrar: ")
-        car_no_perm = ['!', '#', '$', '%', '&', '*', '?']
-        while (
+        car_no_perm = ['!', '#', '$', '%', '&', '*', '?']  # defino una variable con los caracteres no permitidos que
+        # vamos a usar tanto en usuario como en contraseña
+        while (  # he agrupado todas las condiciones a cumplir con el while
                 len(username) <= 3 or
                 '@' not in username or
                 not (username.endswith('.com') or username.endswith('.es') or username.endswith('.net')) or
@@ -66,11 +30,11 @@ while True:
         ):
             print(
                 "El usuario debe tener más de 3 caracteres, contener '@', terminar en .com, .net o .es y no incluir caracteres especiales (!, #, $, %, &, *, ?)")
-            username = input("Introduce un nombre de usuario válido: ")
+            username = input("Introduce un nombre de usuario válido: ")  # mensaje de que las condic son válidas
 
         print(f"Usuario {username} válido")
         password = input("Introduce la nueva contraseña: ")
-        while (
+        while (  # igual que en el usuario, agrupo las condiciones
                 len(password) < 8 or
                 not any(c.isupper() for c in password) or
                 not any(c.isnumeric() for c in password) or
@@ -80,7 +44,7 @@ while True:
                 "La contraseña debe tener al menos 8 caracteres, incluir una mayúscula, una minúscula y un carácter especial (!, @, #, $, %, &, *, ?).")
             password = input("Introduce una nueva contraseña: ")
             print("Contraseña creada correctamente")
-
+        # con el append unimos el usuario y la contraseña al array vacío que hemos declarado antes del while
         usu_reg.append(username)
         cont_reg.append(password)
         print(f"´{usu_reg} registrado correctamente.")
@@ -90,6 +54,8 @@ while True:
         while (cont <= 3):
             usu_comp = input("Introduce el usuario con el que quieres iniciar sesión: ")
             cont_comp = input("Introduce su contraseña: ")
+            # vamos a comprobar que el usuario esté y después a comprobar en qué posición del array está
+            # y que la contraseña de esa posición coincida
             if usu_comp in usu_reg:
                 indice = usu_reg.index(
                     usu_comp)  # vamos a llamar al indice del array creado antes para comparar posiciones
