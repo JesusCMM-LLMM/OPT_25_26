c7a2a5d0bdf9958fd1330e00366d10b56b7862e8
JesusCMM <jesuscmoramesa@gmail.com>
2025-10-31 19:11:09 +0100

    redo

--- BLOQUE_01/UD_1_8/b1_10_login.py
+++ BLOQUE_01/UD_1_8/b1_10_login.py
@@ -1,78 +1,82 @@
 """
- Este es el menú sin funciones. Lo primero, para no usar funciones y que solo se cierre al usar la opcion 3, el menú está dentro de un while True.
- Segundo, los arrays vacíos van fuera del bucle para que no se reinicien en cada vuelta.
- NO HEMOS USADO FUNCIONES aunque venía así en la rúbrica de la entrega porque no estaban dentro de este bloque 01
-"""
+Programa de registro, no podemos usar funciones, ni diccionarios, ni arrays, ni tuplas.
+Solo va a recibir un usuario con su contraseña y comprobaremos si se loguea o no
 
-usu_reg = []
-cont_reg = []
+"""
+username = "" #El motivo de que estén declaradas vacías aquí es para despues usarlas de comprobacion en el login
+password = "" #Por si alguien se salta el paso 1
 
 while True:
     print("|######################################|")
-    print("|#############  MENÚ   ################|")
+    print("|#############   MENÚ   ################|")
     print("|######################################|")
     print("| [1].- Registrarse                    |")
     print("| [2].- Iniciar sesión                 |")
     print("| [3].- Salir                          |")
     print("|######################################|")
 
-    # La opción como una variable que va a leerse en el if/elif/else
-    opt = int(input("       Introduce tu opción: "))
+    # Meto el input, la opcion, en un try-except por si el usuario escribe cualquier otra cosa en vez de un número.
+    # Esto hace redundante el else al final
+    try:
+        opt = int(input("        Introduce tu opción: "))
+    except ValueError:
+        print("Opción no válida. Introduce solo un número (1, 2 o 3).")
+        continue # Esto nos manda al inicio del while True
 
     if (opt == 1):
         username = input("Introduce usuario a registrar: ")
-        car_no_perm = ['!', '#', '$', '%', '&', '*', '?']  # defino una variable con los caracteres no permitidos que
-        # vamos a usar tanto en usuario como en contraseña
-        while (  # he agrupado todas las condiciones a cumplir con el while
-                len(username) <= 3 or
-                '@' not in username or
-                not (username.endswith('.com') or username.endswith('.es') or username.endswith('.net')) or
-                any(c in username for c in car_no_perm)
+        car_no_perm_usu = ['!', '#', '$', '%', '&', '*', '?'] # Caracteres no permitidos en el usuario
+
+        while ( #agrupo todas las condiciones con or aunque no sea lo más óptimo
+            len(username) <= 3 or
+            '@' not in username or
+            not (username.endswith('.com') or username.endswith('.es') or username.endswith('.net')) or
+            any(c in username for c in car_no_perm_usu)
         ):
-            print(
-                "El usuario debe tener más de 3 caracteres, contener '@', terminar en .com, .net o .es y no incluir caracteres especiales (!, #, $, %, &, *, ?)")
-            username = input("Introduce un nombre de usuario válido: ")  # mensaje de que las condic son válidas
+            print("El usuario debe tener más de 3 caracteres, contener '@', terminar en .com, .net o .es y no incluir caracteres especiales (!, #, $, %, &, *, ?)")
+            username = input("Introduce un nombre de usuario válido: ")
 
         print(f"Usuario {username} válido")
+        
+        # Ahora pasariamos a los requisitos d econtraseña
+        car_esp_permitidos = ['!', '#', '$', '%', '&', '*', '?', '@'] # Caracteres especiales que si se permiten para la contraseña        
         password = input("Introduce la nueva contraseña: ")
-        while (  # igual que en el usuario, agrupo las condiciones
-                len(password) < 8 or
-                not any(c.isupper() for c in password) or
-                not any(c.isnumeric() for c in password) or
-                not any(c in car_no_perm for c in password)
+        
+        while ( #de nuevo se que quizá no es lo más óptimo, pero creo que la función any() actúa como un flag y no recorre todos los caracteres si no es necesario.
+            len(password) < 8 or
+            not any(c.isupper() for c in password) or 
+            not any(c.isnumeric() for c in password) or
+            not any(c in car_esp_permitidos for c in password) 
         ):
-            print(
-                "La contraseña debe tener al menos 8 caracteres, incluir una mayúscula, una minúscula y un carácter especial (!, @, #, $, %, &, *, ?).")
+            print("La contraseña debe tener al menos 8 caracteres, incluir una mayúscula, una minúscula, un número y un carácter especial (!, @, #, $, %, &, *, ?).")
             password = input("Introduce una nueva contraseña: ")
-            print("Contraseña creada correctamente")
-        # con el append unimos el usuario y la contraseña al array vacío que hemos declarado antes del while
-        usu_reg.append(username)
-        cont_reg.append(password)
-        print(f"´{usu_reg} registrado correctamente.")
+        
+        print("Contraseña creada correctamente") 
+        print(f"Usuario {username} registrado correctamente.") 
 
     elif (opt == 2):
-        cont = 0
-        while (cont <= 3):
-            usu_comp = input("Introduce el usuario con el que quieres iniciar sesión: ")
-            cont_comp = input("Introduce su contraseña: ")
-            # vamos a comprobar que el usuario esté y después a comprobar en qué posición del array está
-            # y que la contraseña de esa posición coincida
-            if usu_comp in usu_reg:
-                indice = usu_reg.index(
-                    usu_comp)  # vamos a llamar al indice del array creado antes para comparar posiciones
-                if cont_comp == cont_reg[indice]:
-                    print("Acceso concedido")
-                    break
-                else:
-                    cont += 1
-                    print("Contraseña incorrecta. Inténtalo de nuevo.")
-                    if cont == 3:
-                        print("Demasiados intentos fallidos. Regresando al menú principal.")
+        if not username: # Comprueba si la variable username está vacía
+            print("No hay ningún usuario registrado. Por favor, regístrese primero (Opción 1).")
+        else: # Si hay usuario, ya te lo pide
+            usu_comp = input("Introduce tu usuario: ")
+            if usu_comp == username:
+                cont = 0 #el contador que cuando a llegue a 3 te dejará sin intentos
+                acceso_concedido = False # flag de acceso                
+                while (cont < 3):
+                    cont_comp = input("Usuario correcto. Introduce tu contraseña: ")
+                    if cont_comp == password:
+                        print("Acceso concedido")
+                        acceso_concedido = True
+                        break # Rompe el bucle de intentos porque se concede acceso
+                    else: # La contraseña es incorrecta, se suma 1 al contador y vuelta al bucle
+                        cont += 1
+                        print(f"Contraseña incorrecta. Te quedan {3 - cont} intentos.")
+                if acceso_concedido == False and cont == 3: #Si no se ha accedido y el contador ya está en 3, patrás
+                    print("Demasiados intentos fallidos. Regresando al menú principal.")
+            
             else:
                 print("Usuario no encontrado. Acceso denegado")
-                break
+
     elif (opt == 3):
         print("Saliendo..")
         exit()
-    else:
-        print("Opción incorrecta")
